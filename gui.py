9e64beb1917626f663e167ad14ab838d7bc24b92
Danil <memorex900@gmail.com>
2019-04-24 23:37:17 +0300

    save

--- gui.py
+++ gui.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3.6
 
 import sys
+import time
 
 from PySide2.QtWidgets import QApplication, QLabel, QWidget, QGraphicsScene, QGraphicsView
-from PySide2.QtWidgets import QPushButton, QHBoxLayout
+from PySide2.QtWidgets import QPushButton, QHBoxLayout, QVBoxLayout
+
 from PySide2.QtCore import Slot
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QRectF
 
 WIDTH = 1200
 HEIGHT = 750
 
+
+def MainWindow(QWidget):
+    def __init__(self, parent=None):
+
+
+@Slot()
+def draw(scene):
+    scene.addRect(200, 200, 100, 100, brush=Qt.blue)
+    scene.update()
 
 def main():
     app = QApplication(sys.argv)
@@ -18,31 +29,49 @@ def main():
         
 
     scene = QGraphicsScene()
-    scene.addText("Hello, world!")
     
     
+
     #scene.
 
     view = QGraphicsView(scene)
         
-    view.setSceneRect(0, 0, 900, 900)
-    button = QPushButton("Run")
-    
-    scene.setBackgroundBrush(Qt.green)
+    view.setSceneRect(0, 0, 500, 500)
 
-    layout = QHBoxLayout()
+    size = view.sceneRect()
+    print(size.bottomRight())
+    print(size.topLeft())
+     
+    button = QPushButton("Step")
+    
+    button.clicked.connect(draw) 
+
+    scene.addRect(0, 0, 100, 100, brush=Qt.gray)
+    scene.addRect(400, 400, 100, 100, brush=Qt.green)
+
+    layout = QVBoxLayout()
     layout.addWidget(view)
     layout.addWidget(button)
-    layout.addStretch(1)
+    #layout.addStretch(1)
 
     window = QWidget()
-    window.resize(1000, 1000)
     window.setLayout(layout)
+    #window.resize(500, 500)
+    #window.setFixedSize(window.size())
+
+    
+    
+    
     window.setWindowTitle('Simple')
 
     window.show()
+    
+    
+    
     app.exec_()
+    
 
+    
 if __name__ == '__main__':
     main()
 
