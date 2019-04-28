bbdfd4d29aa0bff7e3bf9bc1e58d2ee8a5f6f4ba
Danil <memorex900@gmail.com>
2019-04-28 15:38:12 +0300

    simple visualization implemented

--- gui.py
+++ gui.py
@@ -3,74 +3,105 @@
 import sys
 import time
 
-from PySide2.QtWidgets import QApplication, QLabel, QWidget, QGraphicsScene, QGraphicsView
+from PySide2.QtWidgets import QApplication, QLabel, QWidget, QGraphicsScene
 from PySide2.QtWidgets import QPushButton, QHBoxLayout, QVBoxLayout
-
+from PySide2.QtWidgets import QGraphicsView
 from PySide2.QtCore import Slot
 from PySide2.QtCore import Qt, QRectF
 
-WIDTH = 1200
-HEIGHT = 750
+import engine
 
 
-def MainWindow(QWidget):
+class MainWindow(QWidget):
     def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        self.view = QGraphicsView()
+        self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.button = QPushButton("Step")
+        self.button.clicked.connect(self.step) 
+
+        self.button2 = QPushButton("Run")
+        self.button2.clicked.connect(self.run) 
 
 
-@Slot()
-def draw(scene):
-    scene.addRect(200, 200, 100, 100, brush=Qt.blue)
-    scene.update()
+        layout = QVBoxLayout()
+        layout.addWidget(self.view)
+        layout.addWidget(self.button)
+        layout.addWidget(self.button2)
+        
+        self.setLayout(layout) 
+        self.setWindowTitle('Simple')
+        self.setFixedSize(1000, 800)
+        
+        self.scene = QGraphicsScene()
+        self.view.setScene(self.scene)
+        self.scene.setSceneRect(0, 0, self.view.width(), self.view.height())
+        self.view.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+
+        self.universe = engine.Universe(25, 25)
+        f = open('init2', 'r')
+        self.universe.file_input(f)
+        #self.drawGrid()
+        #self.scene.addRect(0, 0, 100, 100, brush=Qt.gray)
+        #self.scene.addRect(400, 400, 100, 100, brush=Qt.green)
+        
+    def drawUniverse(self):
+        width = self.view.width()
+        height = self.view.height()
+        
+        cellWidth = width / self.universe.n
+        cellHeight = height / self.universe.m
+
+        x = 0
+        y = 0
+        """
+        while x < width:
+            y = 0
+            while y < height:
+                self.scene.addRect(x, y, cellWidth, cellHeight, brush=Qt.gray)
+                y += cellHeight
+            x += cellWidth
+        """
+        for i, this_list in enumerate(self.universe.map):
+            y = 0
+            for j, elem in enumerate(this_list):
+                color = Qt.gray
+                if elem == 1:
+                    color = Qt.green
+                self.scene.addRect(x, y, cellWidth, cellHeight, brush=color)
+                y += cellHeight
+            x += cellWidth
+
+    def showEvent(self, event):
+        self.drawUniverse() 
+
+    def step(self):
+        self.scene.clear()
+        self.universe.step()
+        self.drawUniverse()
+        self.scene.update()
+    
+    def run(self):
+        while 1:
+            time.sleep(1)
+            self.step()
+
+
+    def draw(self):
+        self.scene.addRect(200, 200, 100, 100, brush=Qt.blue)
+        self.scene.update()
 
 def main():
     app = QApplication(sys.argv)
     
-    
-        
+    window = MainWindow()
+    window.show()    
 
-    scene = QGraphicsScene()
-    
-    
-
-    #scene.
-
-    view = QGraphicsView(scene)
-        
-    view.setSceneRect(0, 0, 500, 500)
-
-    size = view.sceneRect()
-    print(size.bottomRight())
-    print(size.topLeft())
-     
-    button = QPushButton("Step")
-    
-    button.clicked.connect(draw) 
-
-    scene.addRect(0, 0, 100, 100, brush=Qt.gray)
-    scene.addRect(400, 400, 100, 100, brush=Qt.green)
-
-    layout = QVBoxLayout()
-    layout.addWidget(view)
-    layout.addWidget(button)
-    #layout.addStretch(1)
-
-    window = QWidget()
-    window.setLayout(layout)
-    #window.resize(500, 500)
-    #window.setFixedSize(window.size())
-
-    
-    
-    
-    window.setWindowTitle('Simple')
-
-    window.show()
-    
-    
-    
     app.exec_()
     
-
     
 if __name__ == '__main__':
     main()
